10179035d516008eed9079c4f07b7bd430e4c4c8
Hiroya Onoe <git@onoe.dev>
2022-08-09 23:54:47 +0900

    Force Fix thread_id to board_id on comment

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_144931) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "thread_id"
+    t.integer "board_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

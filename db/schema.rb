ce86750ab19264e372091f197f334e235454c26d
Hiroya Onoe <git@onoe.dev>
2022-08-09 19:33:07 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_100435) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_102018) do
+  create_table "boards", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false

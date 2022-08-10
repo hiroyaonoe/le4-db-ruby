ca1ed01b8f91ec9e4df423d0b1b12540dfdee0c3
Hiroya Onoe <git@onoe.dev>
2022-08-11 01:25:09 +0900

    Generate devise files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   resources :boards
   resources :comments, only: [:create]
   resources :users

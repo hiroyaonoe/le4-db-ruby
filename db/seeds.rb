3bbad24b072697247e7b03be66dba2778ddf4328
Hiroya Onoe <git@onoe.dev>
2022-08-11 01:51:47 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user1 = User.create({ name: "User1" })
+user1 = User.create({ name: "User1", email: "user1@example.com", password: "password1"})
 category1 = Category.create({ name: "Category1" })
 board1 = Board.create({ title: "Board1", user: user1, category: category1 })
 Comment.create([{ content: "comment1", user: user1, board: board1 }, { content: "comment2", user: user1, board: board1 }])

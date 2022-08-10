3fc7d8a14ce1483eb5c35243cfe5ff024e596994
Hiroya Onoe <git@onoe.dev>
2022-08-10 21:43:21 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create({ name: "User1" })
+category1 = Category.create({ name: "Category1" })
+board1 = Board.create({ title: "Board1", user: user1, category: category1 })
+Comment.create([{ content: "comment1", user: user1, board: board1 }, { content: "comment2", user: user1, board: board1 }])

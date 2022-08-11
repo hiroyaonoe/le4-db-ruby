ef63a57e39230cbca32b9b99fe71e6e9a9d95724
Hiroya Onoe <git@onoe.dev>
2022-08-11 16:11:51 +0900

    Fix routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    registrations: "users/registrations"
   }
   devise_scope :user do
     get 'login', to: 'users/sessions#new'

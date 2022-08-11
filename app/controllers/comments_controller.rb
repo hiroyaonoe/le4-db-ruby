0fcc419a0c9bf277596db7521034b7cd1a0c2f8d
Hiroya Onoe <git@onoe.dev>
2022-08-11 17:38:44 +0900

    レンダリングのためにcomment#createでboardを取得しておく

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,27 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: %i[ create ]
+  before_action :set_board, only: %i[ create ]
+
 
   # POST /boards/:board_id/comments
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
-    @comment.board_id = board_id
+    @comment.board = @board
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to board_url(board_id), notice: "Comment was successfully created." }
+        format.html { redirect_to board_url(@board), notice: "Comment was successfully created." }
       else
-        format.html { render "boards/show", status: :unprocessable_entity, location: board_url(board_id) }
+        format.html { render "boards/show", status: :unprocessable_entity, location: board_url(@board) }
       end
     end
   end
 
   private
-    def board_id
-      params[:board_id]
+    def set_board
+      @board = Board.find(params[:board_id])
     end
+
     def comment_params
       params.require(:comment).permit(:content)
     end

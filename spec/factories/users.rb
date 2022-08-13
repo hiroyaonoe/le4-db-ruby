cbc3b1aa7710f26139ee001a6f54d7a020196171
Hiroya Onoe <git@onoe.dev>
2022-08-14 01:44:44 +0900

    Fix factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "User#{n}"}
-    sequence(:email) { |n| "user#{n}@example.com"}
+    sequence(:name) { |n| "User#{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password {"no hashed password"}
     traits_for_enum :role
   end
